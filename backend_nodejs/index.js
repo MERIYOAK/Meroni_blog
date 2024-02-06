f56e8f299edec8c53b124ecb17eca7e433997bb5
meron <merommichaelahw@gmail.com>
2024-02-06 23:48:15 +0100

    mongo url issue

--- backend_nodejs/index.js
+++ backend_nodejs/index.js
@@ -46,14 +46,14 @@ const reqiuredRolesForModeration = process.env.REQUIRED_ROLES_FOR_MODERATION.spl
 const requiredRolesForPostCRUD = process.env.REQUIRED_ROLES_FOR_POST_CRUD.split(',');
 const requiredRolesForReactionCRUD = process.env.REQUIRED_ROLES_FOR_REACTION_CRUD.split(',');
 
-mongoose.connect("process.env.MONGODB_URI").then(() => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.log("Error connecting to MongoDB:", err);
 });
 
 const mongoStore = new MongoStore({
-    mongoUrl: "process.env.MONGODB_URI",
+    mongoUrl: process.env.MONGODB_URI,
     collection: 'sessions',
     ttl: process.env.SESSION_TTL,
 });
@@ -77,7 +77,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/public', express.static(path.join(__dirname, 'public'), { 'Content-Type': 'text/css' }));
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://meroniblog.onrender.com'],
+    origin: ['http://localhost:5173', 'https://meroniblogsite.onrender.com'],
     methods: 'GET, POST, PUT, DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
