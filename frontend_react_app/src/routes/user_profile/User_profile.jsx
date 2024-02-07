d1be5e8eba51df16913cca2484e6f65811c53599
meron <merommichaelahw@gmail.com>
2024-02-07 01:45:20 +0100

    changed user_profile.css spelling

--- frontend_react_app/src/routes/user_profile/User_profile.jsx
+++ frontend_react_app/src/routes/user_profile/User_profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './User_profile.css';
+import './user_profile.css';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import Updater from '../updater/Updater';
