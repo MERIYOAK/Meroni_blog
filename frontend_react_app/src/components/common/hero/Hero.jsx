145a4d16341754dd2d5405a359bae4eeadd3e4a1
meron <merommichaelahw@gmail.com>
2024-03-09 12:31:27 +0100

    added meta tags for social media and some css

--- frontend_react_app/src/components/common/hero/Hero.jsx
+++ frontend_react_app/src/components/common/hero/Hero.jsx
@@ -5,7 +5,7 @@ import { PORTFOLIO_URL } from '../../../../config'
 
 function Hero() {
     return (
-        <div>
+        <div className='hero'>
             <div className='my_image'><img src={Meron} /></div>
             <div className='intro'>
                 <i className='italics'>Hey, I'm</i>
@@ -13,7 +13,7 @@ function Hero() {
                 <h2 className='name'>Michael</h2>
             </div>
             <div className='about_me'>
-                <a className='btn-primary' href={PORTFOLIO_URL} target='_blank'>About me...</a>
+                <a className='btn-primary about-me-btn' href={PORTFOLIO_URL} target='_blank'>About me...</a>
             </div>
         </div>
     )
